38e4f69b62e889c647b2e54e81dd60f0b6984eb6
Stephen <ma000094@umn.edu>
2023-10-24 13:17:19 -0500

    second commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Parse node data from a text file (e.g., coordinates and connections)
+# Create a graph and add nodes and edges
+
+# Implement the A* algorithm
+def astar(graph, start, goal):
+    open_set = PriorityQueue()
+    open_set.put((0, start))
+    came_from = {}
+    g_score = {node: float('inf') for node in graph.nodes()}
+    g_score[start] = 0
+    f_score = {node: float('inf') for node in graph.nodes()}
+    f_score[start] = heuristic(start, goal)
+
+    while not open_set.empty():
+        _, current = open_set.get()
+        if current == goal:
+            return reconstruct_path(came_from, current)
+
+        for neighbor in graph.neighbors(current):
+            tentative_g_score = g_score[current] + distance(graph, current, neighbor)
+            if tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
+                open_set.put((f_score[neighbor], neighbor))
+
+    return None
+
+def heuristic(node, goal):
+    # Define your heuristic function (e.g., Euclidean distance)
+
+def distance(graph, node1, node2):
+    # Define your distance function (e.g., edge weight or cost)
+
+def reconstruct_path(came_from, current):
+    path = [current]
+    while current in came_from:
+        current = came_from[current]
+        path.insert(0, current)
+    return path
+
+
+# Create a Matplotlib figure and axis
+fig, ax = plt.subplots()
+
+# Draw the graph using Matplotlib
+pos = nx.spring_layout(graph)  # Define node positions (you can choose a layout)
+nx.draw(graph, pos, with_labels=True, node_size=300)
+
+# Add labels to nodes (optional)
+labels = {node: f"{node}" for node in graph.nodes()}
+nx.draw_networkx_labels(graph, pos, labels)
+
+# Show the visualization
+plt.show()
+
+# graph = nx.Graph()
+# # Add nodes and edges to the graph
+
+# # Call the A* algorithm
+# start_node = 'A'  # Replace with your actual start node
+# goal_node = 'B'   # Replace with your actual goal node
+# path = astar(graph, start_node, goal_node)
+
+# # Visualize the graph and path
+# pos = nx.spring_layout(graph)  # Adjust layout as needed
+# nx.draw(graph, pos, with_labels=True, node_size=300)
+# nx.draw_networkx_labels(graph, pos, labels={node: node for node in graph.nodes()})
+# if path:
+#     path_edges = [(path[i], path[i + 1]) for i in range(len(path) - 1)]
+#     nx.draw_networkx_edges(graph, pos, edgelist=path_edges, edge_color='r', width=2)
+# plt.show()
